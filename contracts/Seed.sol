c2fa9ba7b56690bbf913b8e1230321fa11711be8
PunkUnknown <PunkUnknown@tutanota.com>
2021-02-22 17:24:10 +0500

    seed contract

--- contracts/Seed.sol
+++ contracts/Seed.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.6;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract Seed is Ownable {
+contract Seed {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
