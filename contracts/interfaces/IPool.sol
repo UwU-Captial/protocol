2aaa2db9b533e53996796bde62b110965450a9d3
PunkUnknown <PunkUnknown@tutanota.com>
2021-04-19 16:15:29 +0500

    removed more redundant contracts

--- contracts/interfaces/IPool.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0;
-
-interface IPool {
-    function rewardDistributed() external returns (uint256);
-}
--- contracts/interfaces/ISeed.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0;
-
-interface ISeed {
-    function totalUwUDistributed() external returns (uint256);
-}
